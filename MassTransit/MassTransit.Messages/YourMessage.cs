c5e14d5e05db25f4dd88a4a7ab8b2f9e18d34f61
Diamond-Dragon <diamond_dragon@tut.by>
2018-01-20 16:59:01 +0300

    Mass transit created

---
+++ MassTransit/MassTransit.Messages/YourMessage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace MassTransit.Messages
+{
+    public class YourMessage
+    {
+        public string Text { get; set; }
+    }
+
+}
---
+++ MassTransit/MassTransit.Publisher/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using MassTransit.Messages;
+
+namespace MassTransit.Publisher
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bus = Bus.Factory.CreateUsingRabbitMq(sbc =>
+            {
+                var host = sbc.Host(new Uri("rabbitmq://localhost"), h =>
+                {
+                    h.Username("guest");
+                    h.Password("guest");
+                });
+
+;
+            });
+
+            bus.Start();
+
+            bus.Publish(new YourMessage { Text = "Hi" });
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+
+            bus.Stop();
+        }
+    }
+}
